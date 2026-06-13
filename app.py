6c953b4c6bf6252de8b5149d50b87004229d5092
sivaprasad <sivaprasadangudu@gmail.com>
2026-06-13 04:17:39 -1000

    added mock users

--- app.py
+++ app.py
@@ -7,23 +7,12 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/env")
-def read_env():
-    if os.getenv("ENV") =="development": 
-        return {"ENV": "Development","message": "This is a development environment"}
-    return {"ENV": "Production"}
-
-r = redis.Redis(
-    host="redis",
-    port=6379,
-    decode_responses=True
-)
-
-@app.get("/visit")
-def visit():
-
-    count = r.incr("visits")
-
+@app.get("/users")
+def get_users():
     return {
-        "visits": count
+        "users": [
+            {"id": 1, "name": "Alice"},
+            {"id": 2, "name": "Bob"},
+            {"id": 3, "name": "Charlie"}
+        ]
     }
