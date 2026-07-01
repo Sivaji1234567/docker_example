a89ee7dbdbf5b1f66c59688c0521c2577b14c749
sivaprasad <sivaprasasangudu@gmail.com>
2026-06-30 20:18:46 -1000

    commented

--- app.py
+++ app.py
@@ -35,12 +35,12 @@ def get_books():
         ]
     }
 
-@app.get("/notes")
-def get_notes():
-    return {
-        "notes": [
-            {"id": 1, "title": "Note 1", "content": "This is the first note."},
-            {"id": 2, "title": "Note 2", "content": "This is the second note."},
-            {"id": 3, "title": "Note 3", "content": "This is the third note."}
-        ]
-    }
+# @app.get("/notes")
+# def get_notes():
+#     return {
+#         "notes": [
+#             {"id": 1, "title": "Note 1", "content": "This is the first note."},
+#             {"id": 2, "title": "Note 2", "content": "This is the second note."},
+#             {"id": 3, "title": "Note 3", "content": "This is the third note."}
+#         ]
+#     }
